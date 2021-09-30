f84cfac9bf6191b31af5fab902d309023d1dbb3d
umeyudai <ume.yudai@gmail.com>
2021-09-30 18:17:12 +0900

    演習問題01

--- ch01/src/exercise/Seiseki.java
+++ ch01/src/exercise/Seiseki.java
@@ -1,5 +1,41 @@
 package exercise;
 
 public class Seiseki {
-    
+    private String number;
+    private String name;
+    private int[] score;
+    private boolean attend;
+
+    public Seiseki(String number, String name, int[] score, boolean attend){
+        this.number = number;
+        this.name = name;
+        this.score = score;
+        this.attend = attend;
+    }
+
+    public String getNumber(){
+        return number;
+    }
+    public String getName(){
+        return name;
+    }
+    public int[] getScore(){
+        return score;
+    }
+    public boolean isAttend(){
+        return attend;
+    }
+
+    public void setNumber(String number){
+        this.number = number;
+    }
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setScore(int[] score){
+        this.score = score;
+    }
+    public void setAttend(boolean attend){
+        this.attend = attend;
+    }
 }
